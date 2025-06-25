48d8481be37e800bd8de94806f2250b1ee2a9fb9
Nattam <nattam.pereira@gmail.com>
2025-06-24 22:46:49 -0300

    serializers: get_rate and validade_description

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,24 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
+
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('rating'))['rating__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
+
+    def validate_description(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError("Description must be 500 characters or less.")
+        return value
