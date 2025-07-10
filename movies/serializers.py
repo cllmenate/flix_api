8c4e43a5486634ebf28fd86d27d2fc5ee4d6c405
Nattam <nattam.pereira@gmail.com>
2025-07-09 23:23:16 -0300

    application: movie dinamic serializer and dev requirements

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,14 +1,30 @@
 from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
+from actors.serializers import ActorSerializer
+from genres.serializers import GenreSerializer
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_description(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError("Description must be 500 characters or less.")
+        return value
+
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True, read_only=True)
+    genre = GenreSerializer(read_only=True)
+    rate = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'description', 'release_date', 'rate', 'actors',]
 
     def get_rate(self, obj):
         rate = obj.reviews.aggregate(Avg('rating'))['rating__avg']
@@ -17,8 +33,3 @@ class MovieSerializer(serializers.ModelSerializer):
             return round(rate, 1)
 
         return None
-
-    def validate_description(self, value):
-        if len(value) > 500:
-            raise serializers.ValidationError("Description must be 500 characters or less.")
-        return value
