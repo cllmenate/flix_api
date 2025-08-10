2f2b7cb00f9b5ba8180cb8b2e547d92d2fbb1025
Nattam <nattam.pereira@gmail.com>
2025-08-10 10:28:32 -0300

    movie stats: here we go again

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count, Avg
-from rest_framework import generics, views, response, status
+from rest_framework import generics, views, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermissionClass
 from movies.models import Movie
@@ -45,7 +46,7 @@ class MovieStatsView(views.APIView):
         data = {
             "message": "Movie statistics",
             "total_movies": total_movies,
-            "movies_per_genre": list(movies_per_genre),
+            "movies_per_genre": movies_per_genre,
             "total_reviews": total_reviews,
             "average_rating": round(average_rating, 1) if average_rating else 0.0,
         }
@@ -53,7 +54,7 @@ class MovieStatsView(views.APIView):
         serializer = MovieStatsSerializer(data=data)
         serializer.is_valid(raise_exception=True)
 
-        return response.Response(
+        return Response(
             data=serializer.validated_data,
             status=status.HTTP_200_OK
         )
